870d07fc94b60b09a7a581a4db0a22d6f8334e43
Jimbo <dextron.js@gmail.com>
2024-03-30 22:57:46 -0400

    creando modelos

--- server/tasks/models.py
+++ server/tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    id_estudiante = models.IntegerField()
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return str(self.id_estudiante)
+
+class Materia(models.Model):
+    id_materia = models.IntegerField()
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=150)
+
+    def __str__(self) -> str:
+        return str(self.id_materia)
+
+class EstMat(models.Model):
+    id_estudiante = models.IntegerField()
+    id_materia = models.IntegerField()
